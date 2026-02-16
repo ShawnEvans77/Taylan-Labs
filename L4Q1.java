b3f38332aa166f09888e8d148c7dc26a01bd1f0c
Shawn Evans <shawnevans328@gmail.com>
2026-02-15 19:04:15 -0500

    knock out females

---
+++ L4Q1.java
@@ -0,0 +1,11 @@
+public class L4Q1 {
+    public static void main(String[] args) {
+        double a = 1;
+        double b = 2;
+        double c = 3;
+        double x = 4;
+        double y = 5;
+
+        System.out.println(((3+4*x)/5) - ((10*(y-5)*(a+b+c))/x) + 9 * ((4/x) + (9+x)/y));
+    }
+}
